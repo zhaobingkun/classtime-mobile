941eba7aefd7ab8f762c94e81dbc7eb3b8174356
zhaobingkun <zhaobingkun@139.com>
2016-07-11 22:29:25 +0800

    添加课程功能

--- src/main/java/com/classtime/mobile/controller/ClassController.java
+++ src/main/java/com/classtime/mobile/controller/ClassController.java
@@ -1,14 +1,17 @@
 package com.classtime.mobile.controller;
 
 import com.classtime.mobile.util.CookieUtil;
+import com.classtime.service.manager.ClassTimeChildManager;
 import com.classtime.service.manager.ClassTimeMainManager;
 import com.classtime.service.manager.StudentManager;
+import com.classtime.service.model.ClassTimeChild;
 import com.classtime.service.model.ClassTimeMain;
 import com.classtime.service.model.Cpsuser;
 import com.classtime.service.model.Student;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.ModelAttribute;
 import org.springframework.web.bind.annotation.RequestMapping;
 
 import javax.servlet.http.HttpServletRequest;
@@ -26,6 +29,9 @@ public class ClassController {
 
     @Autowired
     private ClassTimeMainManager classTimeMainManager;
+
+    @Autowired
+    private ClassTimeChildManager classTimeChildManager;
 
 
     /**
@@ -73,20 +79,22 @@ public class ClassController {
 
 
     /**
-     * 课程添加页面，添加成功后转到课程列表页
+     * 课程添加页面，添加主表，批量添加从表。添加成功后转到课程列表页
      * @param request
-     * @param model
+     * @param pageModel
      * @return
      */
 
     @RequestMapping("classadd.html")
-    public String classadd(HttpServletRequest request, Model model) {
+    public String classadd(HttpServletRequest request, @ModelAttribute ClassTimeMain pageModel) {
         Cpsuser cpsuser = CookieUtil.getUserFromCookie(request);
-        List<Student> studentList = studentManager.selectForUser(Integer.parseInt(cpsuser.getId()+""));
+        //List<Student> studentList = studentManager.selectForUser(Integer.parseInt(cpsuser.getId()+""));
         ClassTimeMain classTimeMain = new ClassTimeMain();
-        classTimeMainManager.insert(classTimeMain);
-        model.addAttribute("studentList",studentList);
-        return "classlist";
+        classTimeMainManager.insertSelective(classTimeMain);
+        ClassTimeChild classTimeChild = new ClassTimeChild();
+        classTimeChildManager.insertSelective(classTimeChild);
+        //model.addAttribute("studentList",studentList);
+        return "classlist.html";
     }
 
 }
